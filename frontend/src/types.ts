bb9bb029a2421302ab1e8f48b5a35e884e24e92d
Noe <noeliacarcelle@gmail.com>
2026-05-27 18:08:02 -0300

    Crea nuevo archivo para tipos y los copia a él. Todavía no los borra de app.ts

---
+++ frontend/src/types.ts
@@ -0,0 +1,39 @@
+import { structure } from "./structure.js";
+
+type TypeMap = {
+  string: string;
+  number: number;
+  boolean: boolean;
+  date: Date;
+};
+
+type MyTypeNames = keyof TypeMap;
+
+type ColumnDef = {
+  type: MyTypeNames;
+  label?: string;
+  input?: 'text' | 'email' | 'date' | 'number' | 'textarea' | 'select';
+  options?: Array<{ value: string; label: string }>;
+  required?: boolean;
+  editable?: boolean;
+  readonlyOnEdit?: boolean;
+  nullable?: boolean;
+}
+
+type TableStructure = {
+  columns: Record<string, ColumnDef>
+  pk: string | string[]
+  uiName: string
+  title?: string
+  addButtonLabel?: string
+}
+
+type InferType<FieldDefs extends Record<string, ColumnDef>> = {
+  [K in keyof FieldDefs]: TypeMap[FieldDefs[K]['type']]
+}
+
+type TableKey = keyof typeof structure.tables;
+
+type TableRecordMap = {
+  [T in keyof typeof structure.tables]: InferType<(typeof structure.tables)[T]['columns']>
+};
