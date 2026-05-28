8d29a8ce3ad1f82c7897b9571503b9bd1e2d35bf
TobySeirgalea <tobySeirgalea@gmail.com>
2026-05-28 15:29:53 -0300

    Mueve a src de shared sus subcarpetas

--- frontend/src/utils/utils.ts
+++
@@ -1,7 +0,0 @@
-import { structure } from "../ssot/structure.js";
-import type { TableKey } from "../types/types.js";
-
-export function getPkFields(tableKey: TableKey): string[] {
-  const tableConfig = structure.tables[tableKey];
-  return Array.isArray(tableConfig.pk) ? tableConfig.pk : [tableConfig.pk];
-}
